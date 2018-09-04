c4a698887508d0d4aad9b1d929143263d4c7c50e
Timchenko Ilya <skiny66.ilya@gmail.com>
2018-09-04 14:31:08 +0600

    fix dru-monads version and fix specs

--- spec/fcb/behavioral_scoring_spec.rb
+++ spec/fcb/behavioral_scoring_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe FCB::BehavioralScoring do
 
     it "returns behavioral scoring" do
       request = FCB::BehavioralScoring.new(env: :test, user_name: ENV["FCB_TEST_USERNAME"], password: ENV["FCB_TEST_PASSWORD"])
-      result = request.call(iin: "820108350867")
+      result = request.call(iin: "730724301968")
       expect(result).to be_a(Dry::Monads::Result::Success)
     end
   end
